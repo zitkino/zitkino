ecdc19175b71422e2355e8bec16804271b9989d2
Jan Hermann <hermajan@gmail.com>
2016-10-14 16:01:01 +0200

    Added Kinokavarna parser.

--- app/model/Movie.php
+++ app/model/Movie.php
@@ -5,7 +5,7 @@ namespace Zitkino;
  * Movie
  */
 class Movie {
-	private $name, $datetime;
+	private $name, $datetime, $link;
 	
 	function getName() {
 		return $this->name;
@@ -13,12 +13,18 @@ class Movie {
 	function getDatetime() {
 		return $this->datetime;
 	}
+	function getLink() {
+		return $this->link;
+	}
 
 	function setName($name) {
 		$this->name = $name;
 	}
 	function setDatetime($datetime) {
 		$this->datetime = $datetime;
+	}
+	function setLink($link) {
+		$this->link = $link;
 	}
 
 	public function __construct($name, $datetime) {
---
+++ app/model/parsers/KinokavarnaParser.php
@@ -0,0 +1,65 @@
+<?php
+namespace Zitkino\parsers;
+use DOMDocument, DOMXPath;
+
+/**
+ * KinokavarnaParser
+ */
+class KinokavarnaParser extends Parser {
+	private $url = "http://www.kinokavarna.cz/program.html";
+	private $doc;
+	private $movies = [];
+	
+	function getUrl() {
+		return $this->url;
+	}
+	function getMovies() {
+		return $this->movies;
+	}
+
+	function setUrl($url) {
+		$this->url = $url;
+	}
+	function setMovies($movies) {
+		$this->movies = $movies;
+	}
+	
+	public function __construct() {
+		$this->doc = new DOMDocument("1.0", "UTF-8");
+		$this->doc->formatOutput = true;
+		$this->doc->preserveWhiteSpace = true;
+		
+		$this->getContent();
+	}
+	
+	public function getContent() {
+		$handle = curl_init($this->url);
+		curl_setopt($handle, CURLOPT_RETURNTRANSFER, true);
+		$html = curl_exec($handle);
+		libxml_use_internal_errors(true); // Prevent HTML errors from displaying
+		$this->doc->loadHTML($html);
+		$xpath = new DOMXPath($this->doc);
+		
+		$movieQuery = "//div[@class='aktuality'][count(p)>4]";
+		$events = $xpath->query("//div[@id='content-in']".$movieQuery);
+		
+		$i = 0;
+		foreach($events as $event) {
+			$movieEvent = $xpath->query("//p[@class='MsoNormal']", $event);
+			if($movieEvent->length>4) {
+				$nameQuery = $xpath->query($movieQuery."//h4", $event);
+				$name = mb_substr($nameQuery->item($i)->nodeValue, 10);
+				
+				$date = $xpath->query($movieQuery."//h4//span", $event);
+				$timeQuery = $xpath->query($movieQuery."//p[@class='start']", $event);
+				$time = mb_substr($timeQuery->item($i)->nodeValue, 9, 5);
+				$time = str_replace(".", ":", $time);
+				$datetime = $date->item($i)->nodeValue." ".$time;
+				
+				$this->movies[] = new \Zitkino\Movie($name, $datetime);
+				$this->movies[count($this->movies)-1]->setLink($this->url);
+			}
+			$i++;
+		}
+	}
+}
