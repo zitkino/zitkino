4c21fba845f5ece07a119c42bc0ec0e3955fe862
Jan Hermann <hermajan@gmail.com>
2016-11-08 01:33:21 +0100

    Fixed character in name of the movie in KinokavarnaParser.

--- app/model/parsers/KinokavarnaParser.php
+++ app/model/parsers/KinokavarnaParser.php
@@ -23,16 +23,22 @@ class KinokavarnaParser extends Parser {
 			$movieEvent = $xpath->query("//p[@class='MsoNormal']", $event);
 			if($movieEvent->length>4) {
 				$nameQuery = $xpath->query($movieQuery."//h4", $event);
-				$name = mb_substr($nameQuery->item($i)->nodeValue, 10);
+				if($i == 0) {
+					$startCharacter = 9;
+				}
+				else {
+					$startCharacter = 10;
+				}
+				$name = mb_substr($nameQuery->item($i)->nodeValue, $startCharacter);
 				
 				$link = "http://www.kinokavarna.cz/program.html";
 				
-				$date = $xpath->query($movieQuery."//h4//span", $event);
+				$dateQuery = $xpath->query($movieQuery."//h4//span", $event);
 				$timeQuery = $xpath->query($movieQuery."//p[@class='start']", $event);
 				$timeString = mb_substr($timeQuery->item($i)->nodeValue, 9, 5);
 				$time = str_replace(".", ":", $timeString);
 				
-				$datetime = \DateTime::createFromFormat("j.n.Y", $date->item($i)->nodeValue);
+				$datetime = \DateTime::createFromFormat("j.n.Y", $dateQuery->item($i)->nodeValue);
 				$datetime->setTime(intval(substr($time, 0, 2)), intval(substr($time, 3, 2)));
 				
 				$this->movies[] = new \Zitkino\Movie($name, $datetime);
