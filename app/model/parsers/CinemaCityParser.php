d3aafa93cfdf5f6b175d77cbb046456edb99077e
Jan Hermann <hermajan@gmail.com>
2016-12-25 15:05:21 +0100

    Fixed Olympia Christmas closure.

--- app/model/parsers/CinemaCityParser.php
+++ app/model/parsers/CinemaCityParser.php
@@ -43,55 +43,62 @@ abstract class CinemaCityParser extends Parser {
 		$events = $xpath->query("//table[@id[starts-with(.,'scheduleTable_')]]/tbody/tr");
 		$movieItems = 0;
 		foreach($events as $event) {
-			$nameQuery = $xpath->query("//td[@class='featureName']//a", $event);
-			$name = $nameQuery->item($movieItems)->nodeValue;
-			
-			$link = "http://cinemacity.cz/".$nameQuery->item($movieItems)->getAttribute("href");
-			
-			$type = null;
-			if(strpos($name, "3D") !== false) {
-				$type = "3D";
-				$name = str_replace(" 3D", "", $name);
+			$emptyQuery = $xpath->query(".//td[@class='empty']", $event);
+			if($emptyQuery->length !== 0) {
+				$this->movies = null;
+				break;
 			}
-			
-			$languageQuery = $xpath->query(".//td[4]", $event);
-			$language = $languageQuery->item(0)->nodeValue;
-			if(strpos($language, "CZ") !== false) {
-				$language = "česky";
+			else {
+				$nameQuery = $xpath->query("//td[@class='featureName']//a", $event);
+				$name = $nameQuery->item($movieItems)->nodeValue;
+
+				$link = "http://cinemacity.cz/" . $nameQuery->item($movieItems)->getAttribute("href");
+
+				$type = null;
+				if(strpos($name, "3D") !== false) {
+					$type = "3D";
+					$name = str_replace(" 3D", "", $name);
+				}
+
+				$languageQuery = $xpath->query(".//td[4]", $event);
+				$language = $languageQuery->item(0)->nodeValue;
+				if(strpos($language, "CZ") !== false) {
+					$language = "česky";
+				}
+				if(strpos($language, "EN") !== false) {
+					$language = "anglicky";
+				}
+
+				$subtitlesQuery = $xpath->query(".//td[3]", $event);
+				$subtitles = $subtitlesQuery->item(0)->nodeValue;
+				if(strpos($subtitles, "ČT") !== false) {
+					$subtitles = "české";
+				}
+				if((strpos($subtitles, "DAB") !== false) or (strpos($subtitles, "CZ") !== false)) {
+					$subtitles = null;
+					$language = "česky";
+				}
+
+				$timeQuery = $xpath->query(".//td[@class='prsnt']/a", $event);
+				$datetimes = [];
+				$i = 0;
+				foreach($timeQuery as $timeElement) {
+					$time = explode(":", trim($timeElement->textContent));
+
+					$datetime = \DateTime::createFromFormat("j/m/Y", $this->date);
+					$datetime->setTime(intval($time[0]), intval($time[1]));
+
+					$datetimes[] = $datetime;
+					$i++;
+				}
+
+				$this->movies[] = new \Zitkino\Movie($name, $datetimes);
+				$this->movies[count($this->movies) - 1]->setLink($link);
+				$this->movies[count($this->movies) - 1]->setType($type);
+				$this->movies[count($this->movies) - 1]->setLanguage($language);
+				$this->movies[count($this->movies) - 1]->setSubtitles($subtitles);
+				$movieItems++;
 			}
-			if(strpos($language, "EN") !== false) {
-				$language = "anglicky";
-			}
-			
-			$subtitlesQuery = $xpath->query(".//td[3]", $event);
-			$subtitles = $subtitlesQuery->item(0)->nodeValue;
-			if(strpos($subtitles, "ČT") !== false) {
-				$subtitles = "české";
-			}
-			if((strpos($subtitles, "DAB") !== false) or (strpos($subtitles, "CZ") !== false)) {
-				$subtitles = null;
-				$language = "česky";
-			}
-			
-			$timeQuery = $xpath->query(".//td[@class='prsnt']/a", $event);
-			$datetimes = [];
-			$i = 0;
-			foreach($timeQuery as $timeElement) {
-				$time = explode(":", trim($timeElement->textContent));
-				
-				$datetime = \DateTime::createFromFormat("j/m/Y", $this->date);
-				$datetime->setTime(intval($time[0]), intval($time[1]));
-				
-				$datetimes[] = $datetime;
-				$i++;
-			}	
-			
-			$this->movies[] = new \Zitkino\Movie($name, $datetimes);
-			$this->movies[count($this->movies)-1]->setLink($link);
-			$this->movies[count($this->movies)-1]->setType($type);
-			$this->movies[count($this->movies)-1]->setLanguage($language);
-			$this->movies[count($this->movies)-1]->setSubtitles($subtitles);
-			$movieItems++;
 		}
 		
 		$this->setMovies($this->movies);
