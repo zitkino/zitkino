073e2e1f17365514f5e2a721a496145c8615ba54
Jan Hermann <hermajan@gmail.com>
2017-10-10 01:07:15 +0200

    Fixed Lucerna parser.

--- app/model/parsers/Lucerna.php
+++ app/model/parsers/Lucerna.php
@@ -6,7 +6,7 @@ namespace Zitkino\parsers;
  */
 class Lucerna extends Parser {
 	public function __construct() {
-		$this->setUrl("http://www.kinolucerna.info/index.php/program");
+		$this->setUrl("http://www.kinolucerna.info");
 		$this->initiateDocument();
 		
 		$this->getContent();
@@ -15,48 +15,58 @@ class Lucerna extends Parser {
 	public function getContent() {
 		$xpath = $this->downloadData();
 		
-		$events = $xpath->query("//div[@id='icagenda']//div[@class='event']");
-		$movieItems = 0;
-		foreach($events as $event) {
-			$nameQuery = $xpath->query("//div[@class='eventtitle']//a", $event);
-			$name = $nameQuery->item($movieItems)->nodeValue;
+		$days = $xpath->query("//ul[@id='table_days']//div[@class='scroll-pane-wrapper']//li");
+		foreach($days as $day) {
+			$dayQuery = $xpath->query("./a", $day);
+			$dayId = $dayQuery->item(0)->getAttribute("data-den");
+			$dayString = $dayQuery->item(0)->getElementsByTagName("span")->item(0)->nodeValue;
 			
-			$badNames = array("KINO NEHRAJE – Vánoční prázdniny", "KINO NEHRAJE – Novoroční prázdniny");
-			if(in_array($name, $badNames)) {
-				$movieItems++;
-				continue;
+			$events = $xpath->query("//div[@id='den_".$dayId."']//div[@class='item']");
+			foreach($events as $key => $event) {
+				$info = "./div[@class='heading']";
+				
+				$nameQuery = $xpath->query($info."//h2//a", $event);
+				$name = $nameQuery->item(0)->nodeValue;
+				
+				$link = "http://www.kinolucerna.info".$nameQuery->item(0)->getAttribute("href");
+				
+				$lengthQuery = $xpath->query($info."//div[@class='eventlenght']", $event);
+				$lengthString = $lengthQuery->item(0)->nodeValue;
+				$length = str_replace("&nbsp;min", "", htmlentities($lengthString, null, "utf-8"));
+				
+				$languageQuery = $xpath->query($info."//div[@class='left']/div/span[2]", $event);
+				$languageString = $languageQuery->item(0)->nodeValue;
+				
+				switch(true) {
+					case stripos($languageString, "ČV") !== false: $language = "česky"; $subtitles = null; break;
+					case stripos($languageString, "ČT") !== false: $language = null; $subtitles = "české"; break;
+					case stripos($languageString, "ČD") !== false: $language = "česky"; $subtitles = null; break;
+					default: $language = null; $subtitles = null;
+				}
+				
+				$timesQuery = $xpath->query("./div[@class='times']/div[@class='right']/span/a", $event);
+				
+				if($timesQuery->length == 1) {
+					$priceString = $timesQuery->item(0)->getAttribute("title");
+					$price = str_replace(["Rezervovat vstupenku (", ",- Kč)\nKino sál"], "", $priceString);
+				} else {
+					$timesQuery = $xpath->query("./div[@class='times']/div/span/span", $event);
+					$price = null;
+				}
+				
+				$timeString = $timesQuery->item(0)->nodeValue;
+				$time = explode(":", $timeString);
+				
+				$datetime = \DateTime::createFromFormat("j.m.", $dayString);
+				$datetimes = [$datetime->setTime($time[0], $time[1])];
+				
+				$this->movies[] = new \Zitkino\Movie($name, $datetimes);
+				$this->movies[count($this->movies)-1]->setLink($link);
+				$this->movies[count($this->movies)-1]->setLanguage($language);
+				$this->movies[count($this->movies)-1]->setSubtitles($subtitles);
+				$this->movies[count($this->movies)-1]->setLength($length);
+				$this->movies[count($this->movies)-1]->setPrice($price);
 			}
-			
-			$link = "http://www.kinolucerna.info".$nameQuery->item($movieItems)->getAttribute("href");
-			
-			$languageQuery = $xpath->query(".//div[@class='descshort']", $event);
-			$languageString = $languageQuery->item(0)->nodeValue;
-			$language = "česky";
-			$subtitles = null;
-			if(stripos(strtolower($languageString), "dabing") !== false) {
-				$language = "česky";
-			}
-			if(stripos($languageString, "titulky") !== false) {
-				$language = "anglicky";
-				$subtitles = "české";
-			}
-			
-			$dateQuery = $xpath->query("//div[@class='nextdate']//strong", $event);
-			$datetext = explode(",", $dateQuery->item($movieItems)->nodeValue);
-			
-			$cz = array("leden","únor","březen","duben","květen","červenec","červen","srpen","září","říjen","listopad","prosinec");
-			$en = array("January","February","March","April","May","July","June","August","September","October","November","December");
-			$date = str_replace($cz, $en, $datetext[1]);
-			
-			$datetimes =[];
-			$datetime = \DateTime::createFromFormat(" j. F Y H:i", $date);
-			$datetimes[] = $datetime;
-			
-			$this->movies[] = new \Zitkino\Movie($name, $datetimes);
-			$this->movies[count($this->movies)-1]->setLink($link);
-			$this->movies[count($this->movies)-1]->setLanguage($language);
-			$this->movies[count($this->movies)-1]->setSubtitles($subtitles);
-			$movieItems++;
 		}
 		
 		$this->setMovies($this->movies);
