f421b884472f9fe28fc6cf81728bc8dfb8f3d1de
Jan Hermann <hermajan@gmail.com>
2022-06-20 23:45:11 +0200

    Added translations

--- app/Routers/RouterFactory.php
+++ app/Routers/RouterFactory.php
@@ -19,22 +19,22 @@ class RouterFactory {
 		$router->addRoute("kino[/]", "Cinema:default");
 		$router->addRoute("kino/<id>", "Cinema:profile");
 		
-		$router->addRoute("klasicka[/]", "Cinema:classic");
-		$router->addRoute("klasicky[/]", "Cinema:classic");
-		$router->addRoute("klasicky/program", "Cinema:classic_programme");
-		$router->addRoute("klasicka/program", "Cinema:classic_programme");
+		$router->addRoute("klasicka[/]", ["presenter" => "Cinema", "action" => "type", "type" => "classic"]);
+		$router->addRoute("klasicky[/]", ["presenter" => "Cinema", "action" => "type", "type" => "classic"]);
+		$router->addRoute("klasicky/program", ["presenter" => "Cinema", "action" => "programme", "type" => "classic"]);
+		$router->addRoute("klasicka/program", ["presenter" => "Cinema", "action" => "programme", "type" => "classic"]);
 		
-		$router->addRoute("multiplexy[/]", "Cinema:multiplex");
-		$router->addRoute("multiplex[/]", "Cinema:multiplex");
-		$router->addRoute("multiplexy/program", "Cinema:multiplex_programme");
-		$router->addRoute("multiplex/program", "Cinema:multiplex_programme");
+		$router->addRoute("multiplexy[/]", ["presenter" => "Cinema", "action" => "type", "type" => "multiplex"]);
+		$router->addRoute("multiplex[/]", ["presenter" => "Cinema", "action" => "type", "type" => "multiplex"]);
+		$router->addRoute("multiplexy/program", ["presenter" => "Cinema", "action" => "programme", "type" => "multiplex"]);
+		$router->addRoute("multiplex/program", ["presenter" => "Cinema", "action" => "programme", "type" => "multiplex"]);
 		
-		$router->addRoute("letni[/]", "Cinema:summer");
-		$router->addRoute("letni/program", "Cinema:summer_programme");
+		$router->addRoute("letni[/]", ["presenter" => "Cinema", "action" => "type", "type" => "summer"]);
+		$router->addRoute("letni/program", ["presenter" => "Cinema", "action" => "programme", "type" => "summer"]);
 		
 		// old zitkino addresses
 		$oldCinemas = ["kino-art-brno", "kinokavarna-brno", "kino-lucerna-brno", "kino-scala-brno", "cinema-city-olympia-brno", "cinema-city-velky-spalicek-brno", "letni-kino-na-dvore-mdb-brno"];
-		$newCinemas = ["art", "kinokavarna", "lucerna", "scala", "olympia", "velkySpalicek", "mdb"];
+		$newCinemas = ["art", "kinokavarna", "lucerna", "scala", "olympia", "velky-spalicek", "mdb"];
 		$i = 0;
 		foreach($oldCinemas as $cinema) {
 			$router->addRoute("cinema/".$cinema, ["presenter" => "Cinema", "action" => "profile", "id" => $newCinemas[$i]], Route::ONE_WAY);
