75f6b4a8fc2a5ba0357358bc9e7923eb6c68d6dc
Jan Hermann <hermajan@gmail.com>
2025-07-09 16:17:17 +0200

    Added configuration files and templates for Symfony project

--- assets/app.js
+++ assets/app.js
@@ -1,10 +1,22 @@
+// start the Stimulus application
 import './bootstrap.js';
-/*
- * Welcome to your app's main JavaScript file!
- *
- * This file will be included onto the page via the importmap() Twig function,
- * which should already be in your base.html.twig.
- */
-import './styles/app.css';
+// import './styles/app.scss';
+import 'highlight.js/styles/github-dark-dimmed.css';
+import 'lato-font/css/lato-font.css';
 
-console.log('This log comes from assets/app.js - welcome to AssetMapper! 🎉');
+// loads the Bootstrap plugins
+import 'bootstrap/js/dist/alert';
+import 'bootstrap/js/dist/collapse';
+import 'bootstrap/js/dist/dropdown';
+import 'bootstrap/js/dist/tab';
+import 'bootstrap/js/dist/modal';
+import 'jquery'
+
+// loads the code syntax highlighting library
+import './js/highlight.js';
+
+// Creates links to the Symfony documentation
+import './js/doclinks.js';
+
+import './js/flatpicker.js';
+
